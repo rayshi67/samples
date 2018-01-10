4a4d04aefbb293337efe37345b3f27dc20ce3cd1
Ray Shi <rayshi@Rays-Air.lan>
2018-01-10 21:35:26 +1100

    changes on payroll interface

--- ethereum/payroll/contracts/PayrollInterface.sol
+++ ethereum/payroll/contracts/PayrollInterface.sol
@@ -1,6 +1,6 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.17;
 
-// For the sake of simplicity lets assume EUR is a ERC20 token 
+// For the sake of simplicity lets assume ETHER is the default token for the payment, 
 // Also lets assume we can 100% trust the exchange rate oracle returns
 
 contract PayrollInterface {
@@ -11,43 +11,45 @@ contract PayrollInterface {
         address accountAddress, 
         address[] allowedTokens, 
         uint256 initialYearlyEURSalary
-    );
+    ) public returns(uint256 employeeId);
 
     function setEmployeeSalary(
         uint256 employeeId, 
         uint256 yearlyEURSalary
-    );
+    ) public;
 
-    function removeEmployee(uint256 employeeId);
+    function removeEmployee(uint256 employeeId) public;
 
-    function addFunds() payable; 
+    function addFunds() payable public returns(string); 
+
+    function getEmployeeCount() public constant returns(uint256);
+
+    function getEmployee(uint256 employeeId) public constant
+    	returns(address accountAddress, address[] allowedTokens, uint256 annualSalaryEUR, uint256 lastPayDay); 
     
-    function scapeHatch();
-
-    // function addTokenFunds()? // Use approveAndCall or ERC223 tokenFallback
-
-    function getEmployeeCount() constant returns (uint256);
-
-    function getEmployee(uint256 employeeId) constant returns (address employee); // Return all important info too 
+    function calculatePayrollBurnrate() public constant returns(uint256); // Monthly EUR amount spent in salaries 
     
-    function calculatePayrollBurnrate() constant returns (uint256); // Monthly EUR amount spent in salaries 
-    
-    function calculatePayrollRunway() constant returns (uint256); // Days until the contract can run out of funds 
+    function calculatePayrollRunway() public constant returns(uint256); // Days until the contract can run out of funds 
     
     /* EMPLOYEE ONLY */ 
     
     function determineAllocation(
         address[] tokens, 
         uint256[] distribution
-    ); // only callable once every 6 months 
+    ) public; // only callable once every 6 months 
 
-    function payday(); // only callable once a month
+    function payday() public; // only callable once a month
 
     /* ORACLE ONLY */ 
     
     function setExchangeRate(
       address token, 
       uint256 EURExchangeRate
-    ); // uses decimals from token
+    ) public; // uses decimals from token
+
+	/* Escape Hatch */
+
+	// TODO if time permitted
+    //function escapeHatch(bool escapeHatchInd) public;
 
 }
